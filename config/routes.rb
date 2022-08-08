a9cdfb5ddc3ae4735b6a519ac550a73f03d8f956
hkreds <109258421+hkreds@users.noreply.github.com>
2022-08-08 04:23:48 +0000

    [Add]updateのバリデーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get 'books/edit' => 'books#edit'
   get '/' => 'homes#top'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
