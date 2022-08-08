705199db924390517cc97c924c853b7152c8f598
hkreds <109258421+hkreds@users.noreply.github.com>
2022-08-08 07:33:26 +0000

    [Finish]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get '/' => 'homes#top'
+  root :to  => 'homes#top'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
